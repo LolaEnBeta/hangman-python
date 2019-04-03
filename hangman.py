400b126ca3c07a8e6e663ff239814739b46d78b7
LolaEnBeta <lolitaenbeta@gmail.com>
2019-04-03 20:14:18 +0200

    Initial commit.

---
+++ hangman.py
@@ -0,0 +1,23 @@
+
+word = input("Enter the word:")
+word_length = len(word)
+print(word_length)
+
+x = 8
+while x > 0:
+    letter = input("Write one letter:")
+    letter_position = word.find(letter)
+    print(letter_position)
+    x = x - 1
+print("Game Over!")
+
+
+
+
+
+
+
+
+
+
+        
