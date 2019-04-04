910fdbc52a10f6c03496e69442e85cd768dc0a48
LolaEnBeta <lolitaenbeta@gmail.com>
2019-04-04 17:21:26 +0200

    Print all found letters

--- hangman.py
+++ hangman.py
@@ -1,19 +1,22 @@
 
-word = input("Enter the word:")
-word_length = len(word)
-print(word_length)
+word = input("Enter a word:")
+print(len(word))
+word_letter = list(word)
 
+letter_list = list()
 x = 8
 while x > 0:
     letter = input("Write one letter:")
+    letter_list.append(letter)
     found_letter = False
     #este for recorre cada letra de la palabra.
     for current_letter in word:
+        if current_letter == letter:
+            found_letter = True
         #este if comprueba que la letra de la palabra coincide con la letra input, 
         #si coincide la escribe, sino escribe -.
-        if current_letter == letter:
+        if current_letter in letter_list:
             print(current_letter)
-            found_letter = True
         else:
             print("-")
 
@@ -21,4 +24,5 @@ while x > 0:
     if not found_letter:
         x = x - 1
     print("Te quedan ", x, " oportunidades")
+    print(letter_list)
 print("Game Over!")
