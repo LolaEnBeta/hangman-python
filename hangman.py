93b51f78963c92791fc4deaf9eb0679d0a795152
LolaEnBeta <lolitaenbeta@gmail.com>
2019-04-04 16:22:05 +0200

    Print letter position and oportunities

--- hangman.py
+++ hangman.py
@@ -6,7 +6,19 @@ print(word_length)
 x = 8
 while x > 0:
     letter = input("Write one letter:")
-    letter_position = word.find(letter)
-    print(letter_position)
-    x = x - 1
+    found_letter = False
+    #este for recorre cada letra de la palabra.
+    for current_letter in word:
+        #este if comprueba que la letra de la palabra coincide con la letra input, 
+        #si coincide la escribe, sino escribe -.
+        if current_letter == letter:
+            print(current_letter)
+            found_letter = True
+        else:
+            print("-")
+
+    #esto se tiene que hacer solo si la letra no aparece en la palabra.
+    if not found_letter:
+        x = x - 1
+    print("Te quedan ", x, " oportunidades")
 print("Game Over!")
