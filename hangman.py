2f5f429e2cd2e45a7afbd71787842f4c1d9ba967
LolaEnBeta <lolitaenbeta@gmail.com>
2019-04-05 12:18:02 +0200

    Exit program when reveal all the correct letters

--- hangman.py
+++ hangman.py
@@ -1,14 +1,15 @@
 
 word = input("Enter a word:")
 print(len(word))
-word_letter = list(word)
+word_letter = set(list(word))
 
-letter_list = list()
+letter_list = set(list())
 x = 8
 while x > 0:
     letter = input("Write one letter:")
-    letter_list.append(letter)
+    letter_list.add(letter)
     found_letter = False
+
     #este for recorre cada letra de la palabra.
     for current_letter in word:
         if current_letter == letter:
@@ -20,9 +21,16 @@ while x > 0:
         else:
             print("-")
 
+    #este if compara la lista de la palabra introducida con la creada a partir de input
+    #si es igual: el jugador gana y cierra el programa
+    if letter_list == word_letter:
+        print("You win this time!")
+        exit()
+
     #esto se tiene que hacer solo si la letra no aparece en la palabra.
     if not found_letter:
         x = x - 1
     print("Te quedan ", x, " oportunidades")
     print(letter_list)
+    
 print("Game Over!")
