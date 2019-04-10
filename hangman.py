8c5cf1f4083a4b1fe2ebf636e42ef1d71186f46f
LolaEnBeta <lolitaenbeta@gmail.com>
2019-04-10 12:07:28 +0200

    .

--- hangman.py
+++ hangman.py
@@ -1,9 +1,21 @@
 
+def has_user_won(word, letter_list):
+    matches = 0
+    for letter in word:
+        if letter in letter_list:
+            matches += 1
+
+    if matches == len(word) :
+        return True
+    else:
+        return False
+
 word = input("Enter a word:")
 print(len(word))
 word_letter = set(list(word))
+print(word_letter)
 
-letter_list = set(list())
+letter_list = set()
 x = 8
 while x > 0:
     letter = input("Write one letter:")
@@ -20,17 +32,18 @@ while x > 0:
             print(current_letter)
         else:
             print("-")
-
-    #este if compara la lista de la palabra introducida con la creada a partir de input
-    #si es igual: el jugador gana y cierra el programa
-    if letter_list == word_letter:
-        print("You win this time!")
-        exit()
-
+   
     #esto se tiene que hacer solo si la letra no aparece en la palabra.
     if not found_letter:
         x = x - 1
+    
     print("Te quedan ", x, " oportunidades")
     print(letter_list)
-    
+
+   #refirige al def y lo ejecuta, si el resultado es True: win and exit
+
+    if has_user_won(word, letter_list):
+        print("You win!!!!")
+        exit()
+
 print("Game Over!")
